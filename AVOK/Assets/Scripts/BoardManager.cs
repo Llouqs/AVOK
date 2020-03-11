30522e11787f9fe4909b1c1b5f2ca1a00cb43179
Llouqs <enot2532@gmail.com>
2020-03-11 20:58:41 +0300

    Start
    I am trying

--- AVOK/Assets/Scripts/BoardManager.cs
+++ AVOK/Assets/Scripts/BoardManager.cs
@@ -3,51 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 public class BoardManager : MonoBehaviour
 {
-    public static BoardManager instance;
-    public List<Sprite> characters = new List<Sprite>(); //список спрайтов элементов
-    public GameObject tile; //префаб (элемент в общем виде)
-    public int xSize, ySize; //размер поля в элементах
-    public GameObject[,] tiles; //массив префабов (считать с левого нижнего угла по столбцу вверх, потом вправо и т.д.)
+    public GameObject tilePrefab; //префаб (элемент в общем виде)
+    public GameObject[] dots;
+    public int width, height; //размер поля в элементах
+    public Tile[,] allTiles; //массив префабов (считать с левого нижнего угла по столбцу вверх, потом вправо и т.д.)
+    public GameObject[,] allDots;
     void Start()
     {
-        instance = GetComponent<BoardManager>();
-        CreateBoard();
+        allTiles = new Tile[width, height];
+        allDots = new GameObject[width, height];
+        SetUp();
     }
-    private void CreateBoard()
+
+    private void SetUp()
     {
-        tiles = new GameObject[xSize, ySize];
-        float startX = transform.position.x;
-        float startY = transform.position.y;
-        for (int x = 0; x < xSize; x++)
-        {
-            for (int y = 0; y < ySize; y++)
-            {
-                GameObject newTile = Instantiate(tile, //размещаем префаб на доске
-                                                 new Vector3(startX + x, startY + y, 0), //в указанной координате
-                                                 tile.transform.rotation); //с таким поворотом
-                tiles[x, y] = newTile; //записываем в массив префабов
-                newTile.transform.parent = transform; 
-                Sprite newSprite = characters[Random.Range(0, characters.Count)]; //берем случайный спрайт
-                newTile.GetComponent<SpriteRenderer>().sprite = newSprite; //присваеваем спрайт текущему префабу
-            }
-        }
-    }
-    private void Reffiling()
-    {
-        for (int x = 0; x < xSize; x++)
-        {
-            for (int y = 0; y < ySize; y++)
-            {
-                if (tiles[x, y] == null)
-                { 
-                //GameObject newTile = Instantiate(tile, //размещаем префаб на доске
-                //                                 //new Vector3(startX + x, startY + y, 0), //в указанной координате
-                //                                 tile.transform.rotation); //с таким поворотом
-                //tiles[x, y] = newTile; //записываем в массив префабов
-                //newTile.transform.parent = transform;
-                //Sprite newSprite = characters[Random.Range(0, characters.Count)]; //берем случайный спрайт
-                //newTile.GetComponent<SpriteRenderer>().sprite = newSprite; //присваеваем спрайт текущему префабу
-                }
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                Vector2 tempPosition = new Vector2(i, j);
+                GameObject tile = Instantiate(tilePrefab, tempPosition, Quaternion.identity);
+                tile.transform.parent = this.transform;
+                tile.name = "( " + i + ", " + j + " )";
+                int dotToUse = Random.Range(0, dots.Length);
+                GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                dot.transform.parent = this.transform;
+                dot.name = "( " + i + ", " + j + " )";
+                allDots[i, j] = dot;
             }
         }
     }
