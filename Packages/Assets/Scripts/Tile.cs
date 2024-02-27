7e3771d4fbba8807df77a2dd740704030f5b80fa
Vladislav Kochetkov <enot2532@gmail.com>
2024-02-27 14:19:35 +0300

    Fix and rework

---
+++ Packages/Assets/Scripts/Tile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Tile : MonoBehaviour
+{
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void Initialize()
+    {
+
+
+
+    }
+
+}
